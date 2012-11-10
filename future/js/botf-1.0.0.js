a6deb81db0eedbb2415c6a2d9ece5d983a089c50
Matthew Merryfull <matthew@merryfull.com>
2012-11-10 18:05:55 +1100

    Updated Reference Paths and added branch checkin
    - Updated jQuery lib
    - Created prototype extension methods
    - Created revised paths for images and resources

---
+++ future/js/botf-1.0.0.js
@@ -0,0 +1,37 @@
+﻿/*
+Author: Matthew Merryfull
+Date:   10/11/2012
+Purpose: Branch of the future. A prototype for Westpac in order to move forward with
+innovative financial services.
+*/
+// User Creds...
+var customerId = 1;
+var customerName = "customer";
+
+$(function () {
+
+    initCheckin();    
+
+});
+
+initCheckin = function() {
+
+    // associate the "click" event to checking a person into a branch
+    $('[data-role="branch-checkin"]').on('click', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'PUT',
+            url: "http://web.botfapi.local/api/CheckIn/" + customerId,
+            data: { Id: customerId, Name: customerName, CheckedIn: !($(this).data("status")) },
+            beforeSend: function () {
+                $(this).find("h2.m-heading").text("Retrieving Status");
+            },
+            success: function(data) {
+                $(this).data("status", parseJSON(data).CheckedIn);
+            }
+        });
+        
+    });
+
+};
